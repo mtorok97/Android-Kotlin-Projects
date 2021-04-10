0bb2974226ac0f3cd8fb9dc6a2f1b7ad6cf6a7fd
mtorok97 <mtorok@edu.bme.hu>
2021-04-10 23:26:15 +0200

    Save http data to List
    Settings for date and data processing

--- HFdemo/app/src/main/java/hu/bme/aut/android/hfdemo/fragment/ResultsFragment.kt
+++ HFdemo/app/src/main/java/hu/bme/aut/android/hfdemo/fragment/ResultsFragment.kt
@@ -15,16 +15,16 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
-import java.util.*
+import java.util.Calendar
 
 class ResultsFragment : Fragment() {
 
     private lateinit var binding: FragmentResultsBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View {
         binding = FragmentResultsBinding.inflate(layoutInflater, container, false)
 
@@ -43,17 +43,17 @@ class ResultsFragment : Fragment() {
         }.build()*/
 
         val retrofit = Retrofit.Builder()
-            .baseUrl("https://api-football-beta.p.rapidapi.com/")
-            .addConverterFactory(MoshiConverterFactory.create())
-            //.client(okHttpClient) //Ez csak akkor kell ha a headeröket itt adjuk hozzá, ha egységes headerök akkor hasznos
-            .build()
+                .baseUrl("https://api-football-beta.p.rapidapi.com/")
+                .addConverterFactory(MoshiConverterFactory.create())
+                //.client(okHttpClient) //Ez csak akkor kell ha a headeröket itt adjuk hozzá, ha egységes headerök akkor hasznos
+                .build()
 
 
         val resultsAPI = retrofit.create(ResultsAPI::class.java)
 
         binding.btnGetResults.setOnClickListener {
 
-            val resultsCall = resultsAPI.getMoney("140", "2020", "2021-04-03")
+            val resultsCall = resultsAPI.getMoney("140", getSeason(), getDate())
 
             resultsCall.enqueue(object : Callback<AllData> {
                 override fun onFailure(call: Call<AllData>, t: Throwable) {
@@ -61,32 +61,74 @@ class ResultsFragment : Fragment() {
                 }
 
                 override fun onResponse(
-                    call: Call<AllData>,
-                    response: Response<AllData>
+                        call: Call<AllData>,
+                        response: Response<AllData>
                 ) {
+                    //Adatok mentése
+                    var homeTeamList = mutableListOf<String>()
+                    var awayTeamList = mutableListOf<String>()
+                    var homeGoalsList = mutableListOf<Int?>()
+                    var awayGoalsList = mutableListOf<Int?>()
+                    //homeTeamList.add("hallo")
+
+
                     val sportResult = response.body()
+                    if (sportResult?.response != null) {
+                        for (element in sportResult.response) {
+                            element.teams?.home?.name?.let { it1 -> homeTeamList.add(it1) }
+                            element.teams?.away?.name?.let { it2 -> awayTeamList.add(it2) }
+                            element.goals!!.home.let { it3 -> homeGoalsList.add(it3) }
+                            element.goals!!.away.let { it4 -> awayGoalsList.add(it4) }
+                            //homeGoalsList.add(element.goals!!.home)
+                            //homeGoalsList.add(null)
+
+                        }
+                    }
                     val homeTeam = sportResult?.response?.get(0)?.teams?.home?.name
                     val awayTeam = sportResult?.response?.get(0)?.teams?.away?.name
                     val homeGoals = sportResult?.response?.get(0)?.goals?.home
                     val awayGoals = sportResult?.response?.get(0)?.goals?.away
 
-                    val homeTeam1 = sportResult?.response?.get(1)?.teams?.home?.name
-                    val awayTeam1 = sportResult?.response?.get(1)?.teams?.away?.name
-                    val homeGoals1 = sportResult?.response?.get(1)?.goals?.home
-                    val awayGoals1 = sportResult?.response?.get(1)?.goals?.away
+                    /*binding.tvData.text =
+                            "$homeTeam ${homeGoals.toString()} - ${awayGoals.toString()} $awayTeam\r\n" +
+                                    "$homeTeam1 ${homeGoals1.toString()} - ${awayGoals1.toString()} $awayTeam1\r\n" +
+                                    "$homeTeam2 ${homeGoals2.toString()} - ${awayGoals2.toString()} $awayTeam2\n" +
+                                    "${Calendar.getInstance().get(Calendar.HOUR_OF_DAY) + 2} ${Calendar.getInstance().get(Calendar.MINUTE)} ${Calendar.getInstance().get(Calendar.SECOND)}"
 
-                    val homeTeam2 = sportResult?.response?.get(2)?.teams?.home?.name
-                    val awayTeam2 = sportResult?.response?.get(2)?.teams?.away?.name
-                    val homeGoals2 = sportResult?.response?.get(2)?.goals?.home
-                    val awayGoals2 = sportResult?.response?.get(2)?.goals?.away
-
-                    binding.tvData.text =
-                        "$homeTeam ${homeGoals.toString()} - ${awayGoals.toString()} $awayTeam\r\n" +
-                                "$homeTeam1 ${homeGoals1.toString()} - ${awayGoals1.toString()} $awayTeam1\r\n" +
-                                "$homeTeam2 ${homeGoals2.toString()} - ${awayGoals2.toString()} $awayTeam2"
+                     */
+                    binding.tvData.text = "${homeTeamList.get(3)} ${homeGoalsList?.get(3)?.toString() ?: ""} - ${awayGoalsList?.get(3)?.toString() ?: ""} ${awayTeamList[3]}\r\n"
                 }
             })
         }
         return binding.root
+    }
+
+    private fun getSeason(): String {
+        //Júliusig az előző 2020-as szezont jelzi ki, augusztól már az új szezon meccseit
+        return if (Calendar.getInstance().get(Calendar.MONTH) + 1 <= 7) {
+            "${Calendar.getInstance().get(Calendar.YEAR) - 1}"
+        } else {
+            "${Calendar.getInstance().get(Calendar.YEAR)}"
+        }
+    }
+
+    private fun getDate(): String {
+        val currentDate = Calendar.getInstance()
+        val currentYear = "${currentDate.get(Calendar.YEAR)}"
+        //Month (Format matters because of API)
+        val currentMonthInt = currentDate.get(Calendar.MONTH) + 1
+        val currentMonth: String = if (currentMonthInt < 10) {
+            "0$currentMonthInt"
+        } else {
+            "$currentMonthInt"
+        }
+        //Day (Format matters because of API)
+        val currentDayInt = currentDate.get(Calendar.DAY_OF_MONTH)
+        val currentDay: String = if (currentDayInt < 10) {
+            "0$currentDayInt"
+        } else {
+            "$currentDayInt"
+        }
+        return "$currentYear-$currentMonth-$currentDay"
     }
 }
