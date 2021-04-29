f07978780e6a1f84df660fdcd5a4d1a2998777ac
mtorok97 <mtorok@edu.bme.hu>
2021-04-30 01:56:13 +0200

    TipActivity UI

--- HFdemo/app/src/main/java/hu/bme/aut/android/hfdemo/DetailTipActivity.kt
+++ HFdemo/app/src/main/java/hu/bme/aut/android/hfdemo/DetailTipActivity.kt
@@ -1,20 +1,42 @@
 package hu.bme.aut.android.hfdemo
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import hu.bme.aut.android.hfdemo.databinding.ActivityDetailTipBinding
 
 class DetailTipActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDetailTipBinding
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailTipBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val args = intent.getStringArrayExtra("MATCH")
-        binding.tvdata3.text= "${args?.get(0)} ${args?.get(1)} ${args?.get(2)} ${args?.get(3)} ${args?.get(
-            4
-        )}"
+        binding.tvDate.text = args?.get(7)
+        Glide.with(this).load(args?.get(5)).into(binding.ivHome)
+        Glide.with(this).load(args?.get(6)).into(binding.ivAway)
+        binding.tvTeamHome.text = args?.get(1)
+        binding.tvTeamAway.text = args?.get(2)
+        binding.tvMytip.text = "My tip: ${
+            when (args?.get(8)) {
+                "Home" -> args?.get(1)
+                "Draw" -> "Draw"
+                "Away" -> args?.get(2)
+                else -> ""
+            } 
+        }"
+        binding.btnHome.setOnClickListener {
+            binding.tvMytip.text = "My tip: ${args?.get(1)}"
+        }
+        binding.btnX.setOnClickListener {
+            binding.tvMytip.text = "My tip: Draw"
+        }
+        binding.btnAway.setOnClickListener {
+            binding.tvMytip.text = "My tip: ${args?.get(2)}"
+        }
     }
 }
